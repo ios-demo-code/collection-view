3eca9d950714d4f586e45dff767a6d12b10080c2
SoengSaravit <soengsaravit@gmail.com>
2017-07-29 08:47:30 +0700

    Initial Commit

---
+++ CollectionView-29-07-2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionView-29-07-2017
+//
+//  Created by Soeng Saravit on 7/29/17.
+//  Copyright © 2017 Soeng Saravit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
